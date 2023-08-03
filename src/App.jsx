48ce2042ca9e83e88f7dab18a887ad56a9f69610
David Pam <niratpam@gmail.com>
2023-08-03 02:40:56 +0100

    Create Recipies Web App and Deploy to Firebase

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="title-row">
+        <h1 className="title">Firebase Recipies</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
